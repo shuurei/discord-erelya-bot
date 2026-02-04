c5604a86420a9ffd5f1b6bb8ac6d9b1111d5fc77
shuurei <lxnnylqs44@gmail.com>
2026-02-04 16:58:41 +0100

    Improve the login message

--- src/client/events/clientReady.ts
+++ src/client/events/clientReady.ts
@@ -3,6 +3,7 @@ import { Event } from '@/structures'
 
 import { startAllJobs } from '@/client/jobs/index.js'
 import prisma from '@/database/db'
+import { createNotifCard } from '@/ui/assets/cards/notifCard'
 
 export default new Event({
     once: true,
@@ -12,7 +13,16 @@ export default new Event({
 
         const lunaria = this.client.guilds.cache.get('1280087771540623413')
         if (lunaria && process.env.ENV === 'PROD') {
-            lunaria.members.cache.get('1175882929826713791')?.send('Je viens de démarrer !')
+            lunaria.members.cache.get('1175882929826713791')?.send({
+                files: [
+                    {
+                        attachment: await createNotifCard({
+                            text: "[Système opérationnel.]",
+                        }),
+                        name: 'info.png'
+                    }
+                ]
+            })
         }
 
         try {
